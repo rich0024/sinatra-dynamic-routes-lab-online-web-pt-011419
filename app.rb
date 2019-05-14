5f227f5a042ff98d85a67889f453448a166d978d
Richard Yance <yance0022@gmail.com>
2019-05-14 02:01:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
-  get '/:operation/:number1/:number2' do 
+  get '/:operation/:number1/:number2' do
     @operation = params[:operation]
     @number1 = params[:number1]
     @number2 = params[:number2]
