38cb546ad57f8d85db67e002b525db0cf9de1eef
Richard Yance <yance0022@gmail.com>
2019-05-14 02:01:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,4 +21,19 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
+  get '/:operation/:number1/:number2' do 
+    @operation = params[:operation]
+    @number1 = params[:number1]
+    @number2 = params[:number2]
+    if @operation == "add"
+      "#{@number1.to_i + @number2.to_i}"
+    elsif @operation == "subtract"
+      "#{@number1.to_i - @number2.to_i}"
+    elsif @operation == "multiply"
+      "#{@number1.to_i * @number2.to_i}"
+    elsif @operation == "divide"
+      "#{@number1.to_i / @number2.to_i}"
+    end
+  end
+
 end
